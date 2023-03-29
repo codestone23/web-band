eb16016c94d730067f31a676de91b7bc7d1d3a4c
Phạm Gia Đạt <84780233+codestone23@users.noreply.github.com>
2023-03-29 23:51:03 +0700

    fixed error

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,9 @@ const btnClose = document.querySelector(".modal-close")
 const btnClick = document.querySelectorAll(".js-button")
 const modalJs = document.querySelector(".modal")
 const modalContainer = document.querySelector(".modal-container")
+let header = document.getElementById("header")
+let mobileMenu = document.getElementById("mobile-menu")
+let headerHeight = header.clientHeight;
 
 function showBuyTickets(){
     modalJs.classList.add("active")
@@ -18,3 +21,14 @@ modalJs.addEventListener("click",hideBuyTickets)
 modalContainer.addEventListener("click",function(e){
     e.stopPropagation();
 })
+
+mobileMenu.onclick = () =>{
+    var isClosed = header.clientHeight === headerHeight;
+    if(isClosed){
+        header.style.height = 'auto'
+    }else{
+        header.style.height = null
+    }
+}
+
+
